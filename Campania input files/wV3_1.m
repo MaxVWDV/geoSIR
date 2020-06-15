5483b98dc6f6dbc0e23a64c954e9f5762ddad3f5
MaxVWDV <53457451+MaxVWDV@users.noreply.github.com>
2020-06-16 02:43:07 +0800

    Add files via upload

---
+++ Campania input files/wV3_1.m
@@ -0,0 +1,20 @@
+% function [inputs,outputs] = geoSIR_wrapper_V2_1()
+%
+% geoSIR, written by Max van wyk de vries and Lekaashree rambabu, 04/2020.
+%
+%
+%This function is used to call on to load parameters, run the model, etc.
+%in an ordered way.
+
+
+
+%% Load input parameters
+[inputs] = V3_1();
+
+
+%% Run the model
+[outputs] = geoSIR_run(inputs);
+
+
+%% Plot the results and save
+geoSIR_plot(inputs,outputs);
